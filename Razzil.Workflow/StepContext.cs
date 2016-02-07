e1d6f31bc0dbcd4942e16a62d2d48da3160514ca
Long Nguyen <ba198647@gmail.com>
2016-02-07 23:28:07 +0800

    add

--- Razzil.Workflow/StepContext.cs
+++ Razzil.Workflow/StepContext.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using Razzil.DataAccess;
 using System.Net.Http;
 using Razzil.DataAccess.Repository;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NLog;
+using OpenQA.Selenium.PhantomJS;
 
 namespace Razzil.Workflow
 {
@@ -18,11 +22,20 @@ namespace Razzil.Workflow
                 var bank = db.Banks.Where(x => x.Name == bankName).FirstOrDefault();
                 if(bank != null)
                 {
+                    Logger = LogManager.GetCurrentClassLogger();
+                    WebDriver = new ChromeDriver();
+                    WaitDriver = new WebDriverWait(WebDriver, new TimeSpan(0, 0, bank.TimeOut.Value));
+                    ShortWaitDriver = new WebDriverWait(WebDriver, new TimeSpan(0, 0, bank.TimeOut.Value / 2));
                     Client = new HttpClient() { Timeout = new TimeSpan(0, 0, bank.TimeOut.Value) };
                 }
             }
         }
-        public HttpClient Client { get; set; }
+
+        public Logger Logger { get; private set; }
+        public ChromeDriver WebDriver { get; private set; }
+        public WebDriverWait WaitDriver { get; private set; }
+        public WebDriverWait ShortWaitDriver { get; private set; }
+        public HttpClient Client { get; private set; }
         public int BankId { get; set; }
         public string LastPage { get; set; }
         public StatusCode StatusCode { get; set; }
---
+++ Razzil.Workflow/Steps/EnterPageStep.cs
@@ -0,0 +1,32 @@
+﻿using Razzil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razzil.Workflow
+{
+    public class EnterPageStep : Step
+    {
+
+        public EnterPageStep(int currentStepId, StepContext context)
+        {
+            Initialize(currentStepId, context);
+        }
+        public override async Task<TransactionResult> Execute()
+        {
+            this.Context.WebDriver.Navigate().GoToUrl(this.Url);
+            this.Context.LastPage = this.Context.WebDriver.PageSource;
+            if (this.Context.LastPage.Contains(this.Sign))
+            {
+                return await base.Execute();
+            }
+            else
+            {
+                return TransactionResult.Failed;
+            }
+        }
+    }
+}
