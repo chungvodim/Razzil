3428c39061721e7822e0f70806285cb9583b6dcf
Long Nguyen <ba198647@gmail.com>
2016-02-14 10:52:59 +0800

    NAME => Name

--- Razzil.Web/Controllers/TransactionTypesController.cs
+++ Razzil.Web/Controllers/TransactionTypesController.cs
@@ -49,7 +49,7 @@ namespace Razzil.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,NAME,Description,Active,CreatedTime,LastUpdatedTime,CreatedByUserID,LastUpdatedByUserID")] TransactionType transactionType)
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Active,CreatedTime,LastUpdatedTime,CreatedByUserID,LastUpdatedByUserID")] TransactionType transactionType)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace Razzil.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,NAME,Description,Active,CreatedTime,LastUpdatedTime,CreatedByUserID,LastUpdatedByUserID")] TransactionType transactionType)
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Active,CreatedTime,LastUpdatedTime,CreatedByUserID,LastUpdatedByUserID")] TransactionType transactionType)
         {
             if (ModelState.IsValid)
             {
